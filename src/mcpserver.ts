a3c084be51040399f5700c0708d47b3c6e474836
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-09-27 23:56:02 +0200

    added ollama integration

--- src/mcpserver.ts
+++ src/mcpserver.ts
@@ -11,5 +11,41 @@ const server = new McpServer({
     },
 });
 
+server.registerTool(
+    "get_current_datetime",
+    {
+        title: "Get current datetime",
+        description: "Get the current systemtime and date",
+        inputSchema: {
+        },
+    },
+    async ({ }) => {
+        const current_datetime = new Date().toString();
+        return {
+            content: [{ type: "text", text: `The current date and time is: ${current_datetime}` }]
+        };
+    },
+
+);
+
+
+server.registerTool(
+    "add_event",
+    {
+        title: "Add Event",
+        description: "Add an Event to the calendar",
+        inputSchema: {
+            title: z.string().describe("The title for the event"),
+            datetime: z.string().describe("The Datetime of the event in format YYYY-MM-DDTHH:MM:SS"),
+        },
+    },
+    async ({ title, datetime }) => {
+        return {
+            content: [{ type: "text", text: `event for ${JSON.stringify({ title: title, datetime: datetime })} created` }]
+        };
+    },
+
+);
+
 const transport = new StdioServerTransport();
 await server.connect(transport);
