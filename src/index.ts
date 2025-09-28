28051e784a299a054becb93912f505a00b57498e
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-09-28 03:34:24 +0200

    added multiple tool support and introduction system prompt

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { callLLM, setupLLM } from "./ollama.js";
 const MODEL = process.env.MODEL || 'llama3.2:3b-instruct-q4_K_M';
 
 await setupLLM(MODEL);
-const messages = await callLLM(MODEL, {role:"user", content:"What time is it?"})
+const messages = await callLLM(MODEL, {role:"user", content:"What time is it?"});
 console.log(messages)
