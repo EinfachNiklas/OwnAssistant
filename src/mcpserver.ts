c192dc93ded64aef337a5309dcbc3194ce9d26cf
EinfachNiklas <x.niklas.kohl.x@gmail.com>
2025-09-28 08:48:14 +0200

    connected google calendar api to mcp server

--- src/mcpserver.ts
+++ src/mcpserver.ts
@@ -1,6 +1,10 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { start } from "node:repl";
 import { z } from "zod";
+import { getEvents, createEvent } from "./mcptools/googleapi.js";
+import { title } from "node:process";
+import { create } from "node:domain";
 
 const server = new McpServer({
     name: "ownassistant-mcp",
@@ -25,6 +29,50 @@ server.registerTool(
         };
     },
 
+);
+
+// Google APIs
+
+server.registerTool(
+    "get_events",
+    {
+        title: "Get Events",
+        description: "Get Events from the Calendar for a certain time period (up to 10)",
+        inputSchema: {
+            startTime: z.string().describe("The start of the selection period strictly in the format YYYY-MM-DDTHH:MM:SS or 'default'. If 'default', the current datetime is used."),
+            endTime: z.string().describe("The end of the selection period strictly in format YYYY-MM-DDTHH:MM:SS or 'default'. The end datetime must be after the start. If 'default', the next year is used."),
+        },
+    },
+    async ({ startTime, endTime }) => {
+        const start = startTime === "default" ? new Date() : new Date(startTime);
+        const end = endTime === "default" ? new Date(new Date().getFullYear() + 1) : new Date(endTime);
+
+        const events = await getEvents(start, end);
+        return {
+            content: [{ type: "text", text: `The upcoming events are ${JSON.stringify(events)}` }]
+        };
+    }
+);
+
+server.registerTool(
+    "create_event",
+    {
+        title: "Create Events",
+        description: "Create an event on the users calendar.",
+        inputSchema: {
+            title: z.string().describe("The title of the event"),
+            start: z.string().describe("The start time of the event in format YYYY-MM-DDTHH:MM:SS"),
+            end: z.string().describe("The end time of the event in format YYYY-MM-DDTHH:MM:SS")
+        }
+    },
+    async ({title, start, end}) => {
+        const startTime = new Date(start);
+        const endTime = new Date(end);
+        const event = createEvent(title, startTime, endTime);
+        return {
+            content: [{ type: "text", text: `Created the event ${JSON.stringify(event)}` }]
+        };
+    }
 );
 
 const transport = new StdioServerTransport();
--- src/mcptools/googleapi.ts
+++ src/mcptools/googleapi.ts
@@ -1,54 +1,56 @@
+import 'dotenv/config';
 import path from 'node:path';
 import process from 'node:process';
 import { calendar_v3, google } from 'googleapis';
 
-const GOOGLE_SHARED_CALEDNAR_ID = process.env.GOOGLE_SHARED_CALENDAR_ID;
+const GOOGLE_SHARED_CALENDAR_ID = process.env.GOOGLE_SHARED_CALENDAR_ID;
+if (!GOOGLE_SHARED_CALENDAR_ID) {
+  throw new Error('GOOGLE_SHARED_CALENDAR_ID is not set but is required');
+}
+
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const CREDENTIALS_PATH = path.join(process.cwd(), 'google_auth.json');
 
 const jwtClient = new google.auth.JWT({
     keyFile: CREDENTIALS_PATH,
     scopes: SCOPES
-}
-)
+});
 const calendar = google.calendar({ version: 'v3', auth: jwtClient });
-async function listEvents() {
+
+export async function getEvents(timeMin: Date, timeMax: Date) {
+    console.info(GOOGLE_SHARED_CALENDAR_ID);
     const result = await calendar.events.list({
-        calendarId: GOOGLE_SHARED_CALEDNAR_ID,
-        timeMin: new Date().toISOString(),
+        calendarId: GOOGLE_SHARED_CALENDAR_ID,
+        timeMin: timeMin.toISOString(),
+        timeMax: timeMax.toISOString(),
         maxResults: 10,
         singleEvents: true,
         orderBy: 'startTime',
     });
     const events = result.data.items;
     if (!events || events.length === 0) {
-        console.log('No upcoming events found.');
+        console.info('No upcoming events found.');
         return;
     }
-    console.log('Upcoming 10 events:');
-
-    for (const event of events) {
-        const start = event.start?.dateTime ?? event.start?.date;
-        console.log(`${start} - ${event.summary}`);
-    }
+    console.info(events);
+    return events;
 }
 
-
-async function createEvent(title:string, start: Date, end: Date) {
+export async function createEvent(title: string, start: Date, end: Date) {
 
     const event: calendar_v3.Schema$Event = {
         summary: title,
-        start: {dateTime: start.toISOString(), timeZone: "Europe/Berlin"},
-        end: {dateTime: end.toISOString(), timeZone: "Europe/Berlin"}
+        start: { dateTime: start.toISOString(), timeZone: "Europe/Berlin" },
+        end: { dateTime: end.toISOString(), timeZone: "Europe/Berlin" }
     }
     calendar.events.insert({
-        calendarId: GOOGLE_SHARED_CALEDNAR_ID,
+        calendarId: GOOGLE_SHARED_CALENDAR_ID,
         requestBody: event
     }, function (err, event) {
         if (err) {
             console.log('There was an error contacting the Calendar service: ' + err);
             return;
         }
-        console.log('Event created: %s', event?.data.summary);
+        return event;
     });
 }
